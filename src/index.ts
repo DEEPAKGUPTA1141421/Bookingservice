d6ae229cd09015e8237aefd056a7ce45533ef395
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2025-02-03 13:43:02 +0000

    add express to repo

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ const port = 4000;
 
 // Basic route to check server
 app.get('/', (req, res) => {
-  res.send('Server is up and running!');
+  res.status(200).json({
+    msg:"Server is up and running!"
+  })
 });
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
