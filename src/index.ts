2d2a7ae900ff6b364bd0adf0bd7f46e379297dd7
surbhikuhar <surbhikuhar07@gmail.com>
2025-02-04 10:25:52 +0530

    check commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 
-
 const app = express();
 const port = 4000;
 
 // Basic route to check server
 app.get('/', (req, res) => {
   res.status(200).json({
-    msg:"Server is up and running!"
+    msg:"Server is up and running from my end!"
   })
 });
 app.listen(port, () => {
