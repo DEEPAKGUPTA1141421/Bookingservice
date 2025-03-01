5e80e8af9245aa5fda86524224726bd8c47017a1
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2025-03-02 04:49:53 +0530

    Update authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -13,9 +13,9 @@ import { authorizeRoles, isAuthenticated } from "../middleware/authorised";
 const router = express.Router();
 
 router.post("/send-otp", sendOtp);
-router.get("/getprofile",isAuthenticated, getUserProfile);
+router.get("/getprofile", isAuthenticated, getUserProfile);
 router.post("/verify-otp", verifyOtp);
-router.post("/edit/:id",isAuthenticated,authorizeRoles("user"), upload.single("profilePicture"), editUser);
+router.post("/edit",isAuthenticated,authorizeRoles("user"), upload.single("profilePicture"), editUser);
 router.post("/logout", isAuthenticated, authorizeRoles("user"), logoutUser);
 router.delete(
   "/delete/:user_id",
