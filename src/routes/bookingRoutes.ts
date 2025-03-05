e006dd9812ec2b85874ce61ae0d39eacfd4e2cb7
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2025-03-06 04:31:06 +0530

    Update bookingRoutes.ts

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -10,7 +10,7 @@ import { authorizeRoles, isAuthenticated } from "../middleware/authorised";
 
 const router = express.Router();
 
-router.post("/", isAuthenticated, authorizeRoles("user"), createBooking); // Create booking from cart
+router.post("/initiate", isAuthenticated, createBooking); // Create booking from cart
 router.get("/", isAuthenticated, authorizeRoles("user"), getBookings); // Get all bookings for a user
 router.put("/:bookingId",isAuthenticated,authorizeRoles("user"), updateBooking); // Update booking details
 router.delete("/:bookingId", isAuthenticated, authorizeRoles("user"), deleteBooking); // Delete booking
