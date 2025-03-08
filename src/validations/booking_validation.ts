a95e415f2ec4d38fd34febaec22b6d9ae8aac9de
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2025-03-08 07:08:24 +0530

    Update booking_validation.ts

--- src/validations/booking_validation.ts
+++ src/validations/booking_validation.ts
@@ -1,35 +1,35 @@
 import * as z from "zod";
 import { objectIdSchema } from "../utils/helper";
 
-
-
 export const createBookingSchema = z.object({
-  date: z
-    .string()
-    .min(1, "Date is required")
-    .regex(/^\d{4}-\d{2}-\d{2}$/, "Invalid date format (YYYY-MM-DD)"),
+  date: z.string().min(1, "Date is required"),
   duration: z.number().min(1, "Duration must be at least 1 minute"),
-  start_time: z
-    .string()
-    .min(1, "Start time is required")
-    .regex(/^([01]\d|2[0-3]):[0-5]\d$/, "Invalid time format (HH:MM)"),
+  start_time: z.string().min(1, "Start time is required"),
   providersList: z
     .array(z.string())
     .min(1, "At least one provider must be selected"),
-  serviceoption:z.string()
+  serviceoption: z.string(),
+  actualService: z.string(),
 });
 
-
 export const updateBookingSchema = z.object({
-  status: z
-    .enum(["initiated", "confirmed", "delivered", "canceled"])
-    .optional(),
-  scheduledTime: z.string().optional(),
-  completedTime: z.string().optional(),
-  address: z.string().min(5, "Address must be at least 5 characters long"),
+  status: z.enum(["confirmed", "delivered", "cancelled"]),
+  address: z.object({
+    current_address: z
+      .string()
+      .min(5, "Address must be at least 5 characters long"),
+    location: z.object({
+      type: z.literal("Point"), // Ensure type is always "Point"
+      coordinates: z.tuple([z.number(), z.number()]), // Validate two-number coordinates (lat, lng)
+    }),
+  }),
+  bookingId: z.string(),
+  pointsUsed: z.string(),
+  modeOfPayment: z.enum(["cash", "net-banking"]),
+  finalPrice: z.number(),
 });
 
 export const acceptBookingSchema = z.object({
   bookingId: objectIdSchema,
-  providerId:objectIdSchema
+  providerId: objectIdSchema,
 });
