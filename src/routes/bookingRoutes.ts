061326e8f24fd9bcb813513c4113f20912638569
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2025-03-08 07:04:21 +0530

    Update bookingRoutes.ts

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -1,19 +1,26 @@
 import express from "express";
 import {
   createBooking,
-  updateBooking,
   getBookings,
   deleteBooking,
   acceptBooking,
+  updateBooking,
+  getBookingDetails,
 } from "../controllers/bookingController";
 import { authorizeRoles, isAuthenticated } from "../middleware/authorised";
 
 const router = express.Router();
 
 router.post("/initiate", isAuthenticated, createBooking); // Create booking from cart
-router.get("/", isAuthenticated, authorizeRoles("user"), getBookings); // Get all bookings for a user
-router.put("/:bookingId",isAuthenticated,authorizeRoles("user"), updateBooking); // Update booking details
-router.delete("/:bookingId", isAuthenticated, authorizeRoles("user"), deleteBooking); // Delete booking
+router.get("/all", isAuthenticated, getBookings); // Get all bookings for a user
+router.post("/confirm-booking", isAuthenticated, updateBooking);
+router.get("/:bookingId", getBookingDetails);
+router.delete(
+  "/:bookingId",
+  isAuthenticated,
+  authorizeRoles("user"),
+  deleteBooking
+); // Delete booking
 router.post("/accept", acceptBooking);
 
 export default router;
