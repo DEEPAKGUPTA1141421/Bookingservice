40904e10f4fa46a6db0e13850b2df4eee62f3e65
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2025-03-15 22:46:46 +0530

    Update authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -6,6 +6,9 @@ import {
   deleteUser,
   logoutUser,
   getUserProfile,
+  verifyAadhaar,
+  sendAadhaarOTP,
+  verifyAadhaarOTP,
 } from "../controllers/authController";
 import upload from "../middleware/upload";
 import { authorizeRoles, isAuthenticated } from "../middleware/authorised";
@@ -16,12 +19,20 @@ router.post("/send-otp", sendOtp);
 router.get("/getprofile", isAuthenticated, getUserProfile);
 router.post("/verify-otp", verifyOtp);
 router.post("/edit",isAuthenticated,authorizeRoles("user"), upload.single("profilePicture"), editUser);
-router.post("/logout", isAuthenticated, authorizeRoles("user"), logoutUser);
+router.post("/logout", isAuthenticated, logoutUser);
 router.delete(
   "/delete/:user_id",
   isAuthenticated,
   authorizeRoles("user"),
   deleteUser
 );
+// Aadhaar Verification
+router.post("/verify-aadhaar", verifyAadhaar);
+
+// Send OTP for Aadhaar
+router.post("/send-aadhaar-otp", sendAadhaarOTP);
+
+// Verify Aadhaar OTP
+router.post("/verify-aadhaar-otp", verifyAadhaarOTP);
 
 export default router;
