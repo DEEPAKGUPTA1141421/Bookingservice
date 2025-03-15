4a3e4742d18e8a15b32cc0c0b9bf5d73b14a1a36
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2025-03-15 22:38:56 +0530

    Update authController.ts

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -12,6 +12,9 @@ import {
   sendOtpSchema,
   verifyOtpSchema,
   editUserSchema,
+  aadhaarOtpSchema,
+  aadhaarSchema,
+  aadhaarOtpVerifySchema,
 } from "../validations/authcontroller_validation";
 import { CheckZodValidation, generateOtp } from "../utils/helper";
 import { IRequest } from "../middleware/authorised";
@@ -39,23 +42,22 @@ export const sendOtp = async (
   next: NextFunction
 ): Promise<void> => {
   try {
-    console.log("frontend hitting me");
+    console.log("now orking")
     const validation = CheckZodValidation(req.body, sendOtpSchema, next);
-    const { phone } = validation.data;
+    let { phone,user_type } = validation.data;
     let role = "";
     let user;
     if (!user) {
       user = await User.findOne({ phone });
-      if (user) role = "user";
+      if (user) role = "User";
     }
     if (!user) {
       user = await ServiceProvider.findOne({ phone });
-      if (user) role = "provider"
-      
+      if (user) role = "ServiceProvider";
     }
     if (!user) {
       user = await Admin.findOne({ phone });
-      if(user)role="admin"
+      if (user) role = "Admin";
     }
     const otp = generateOtp();
     const hashedOtp = crypto.createHash("sha256").update(otp).digest("hex");
@@ -64,12 +66,20 @@ export const sendOtp = async (
     const typeOfOtp = user ? "login" : "sign_up";
 
     if (!user) {
-      user = new User({ phone });
-      await user.save();
+      if (user_type == "User") {
+        user = new User({ phone });
+        user_type="User";
+        await user.save();
+      } else{
+        user = new ServiceProvider({ phone });
+        user_type="ServiceProvider";
+        await user.save();
+      }
     }
 
     await Otp.create({
       user_id: user._id,
+      user_type: role,
       otp_code: hashedOtp,
       expires_at: expiresAt,
       typeOfOtp,
@@ -101,21 +111,27 @@ export const verifyOtp = async (
     let user;
     if (!user) {
       user = await User.findOne({ phone });
-      if (user) role = "user";
+      console.log("user me find", user);
+      if (user) role = "User";
+      console.log("user me find", role);
     }
-    else if (!user) {
+    if (!user) {
+      console.log("provider me ", phone);
       user = await ServiceProvider.findOne({ phone });
-      if (user) role = "provider";
+      if (user) role = "ServiceProvider";
+      console.log("provider me ", user);
     }
-    else if (!user) {
+    if (!user) {
       user = await Admin.findOne({ phone });
-      if (user) role = "admin";
+      if (user) role = "Admin";
+      console.log("admin me ", role);
     }
 
     if (!user) return next(new ErrorHandler("User not found", 404));
-
+    console.log("user", user._id);
     const latestOtp = await Otp.findOne({
       user_id: user._id,
+      user_type: role,
       is_used: false,
     }).sort({ createdAt: -1 });
     if (!latestOtp || new Date(latestOtp.expires_at) < new Date())
@@ -129,13 +145,16 @@ export const verifyOtp = async (
       return next(new ErrorHandler("Invalid OTP", 400));
 
     await Otp.updateOne({ _id: latestOtp._id }, { is_used: true });
-    await User.updateOne({ _id: user._id }, { status: "verified" });
+    if (latestOtp.user_type === "User") {
+      await User.updateOne({ _id: user._id }, { status: "verified" });
+    }
 
     const token = generateToken(user._id.toString());
+    console.log("role", role);
     sendResponse(res, 200, "OTP verified successfully", {
       user: user,
       token: token,
-      role:"provider"
+      role: role,
     });
   } catch (error) {
     const err = error as Error;
@@ -162,7 +181,7 @@ export const editUser = async (
   next: NextFunction
 ): Promise<void> => {
   try {
-    console.log("getting hitt by frontend is more for us",req.body);
+    console.log("getting hitt by frontend is more for us", req.body);
     const validation = await CheckZodValidation(req.body, editUserSchema, next);
     console.log("failed Validation", validation);
     if (validation && !validation.success) {
@@ -186,7 +205,12 @@ export const editUser = async (
 
     const updates: Record<string, any> = {};
     if (name) updates.name = name.trim();
-    if (latitude !== undefined && longitude !== undefined && longitude!=null && latitude!=null) {
+    if (
+      latitude !== undefined &&
+      longitude !== undefined &&
+      longitude != null &&
+      latitude != null
+    ) {
       updates["address.location"] = {
         type: "Point",
         coordinates: [longitude, latitude], // Remember: MongoDB stores [longitude, latitude]
@@ -194,21 +218,22 @@ export const editUser = async (
     }
     if (email) updates.email = email.trim();
     if (phone) updates.phone = phone.trim();
-    if (add_address != undefined && add_address != null) updates.add_address = add_address.trim();
-    console.log("check point")
+    if (add_address != undefined && add_address != null)
+      updates.add_address = add_address.trim();
+    console.log("check point");
     if (role) updates.role = role;
     if (password) updates.password = await bcrypt.hash(password, 10);
     if (profilePicture?.location) updates.image = profilePicture.location;
 
     if (!Object.keys(updates).length)
       return next(new ErrorHandler("No updates provided", 400));
-    console.log("hitting update")
+    console.log("hitting update");
     const user = await User.findByIdAndUpdate(id, updates, { new: true });
     if (!user) return next(new ErrorHandler("User not found", 404));
     console.log("getting response till here");
     sendResponse(res, 200, "User updated successfully", user);
-  } catch (error:any) {
-    console.log(error.message)
+  } catch (error: any) {
+    console.log(error.message);
     next(new ErrorHandler("Internal server error", 500));
   }
 };
@@ -241,5 +266,164 @@ export const getUserProfile = async (
     sendResponse(res, 201, "Profile View", u);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+// Aadhaar Verification
+export const verifyAadhaar = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    // Validate Input
+    sendResponse(res, 201, "Aadhaar verified successfully!", {});
+    return;
+    const parsedData = aadhaarSchema.safeParse(req.body);
+    if (!parsedData.success) {
+      next(new ErrorHandler("Verification failed", 302));
+      return;
+    }
+
+    const { aadhaarNumber } = parsedData.data;
+
+    // Call Aadhaar Verifier API
+    const response = await fetch(
+      `${process.env.AADHAAR_VERIFIER_API_URL}/verify`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.AADHAAR_API_KEY}`,
+        },
+        body: JSON.stringify({ aadhaarNumber }),
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      sendResponse(res, 201, "Aadhaar verified successfully!", { data });
+      return;
+    } else {
+      next(new ErrorHandler(data.message || "Aadhaar verification failed.", 404));
+      return;
+    }
+  } catch (error:any) {
+    console.error("Aadhaar verification error:", error);
+    next(new ErrorHandler(error.message, 501));
+    return;
+  }
+};
+
+// Send OTP to Aadhaar-linked Mobile
+export const sendAadhaarOTP = async (
+  req: Request,
+  res: Response,
+  nex: NextFunction
+): Promise<void> => {
+  try {
+    // Validate Input
+    res
+      .status(200)
+      .json({ success: true, message: "OTP sent successfully!", data: {otp:"123456"} } );
+    return;
+    const parsedData = aadhaarOtpSchema.safeParse(req.body);
+    if (!parsedData.success) {
+       res
+        .status(400)
+         .json({ success: false, message: parsedData.error.errors });
+      return;
+    }
+
+    const { aadhaarNumber } = parsedData.data;
+
+    // Call Aadhaar Verifier API to send OTP
+    const response = await fetch(
+      `${process.env.AADHAAR_VERIFIER_API_URL}/send-otp`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.AADHAAR_API_KEY}`,
+        },
+        body: JSON.stringify({ aadhaarNumber }),
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+       res
+        .status(200)
+         .json({ success: true, message: "OTP sent successfully!", data });
+      return;
+    } else {
+      res.status(400).json({
+        success: false,
+        message: data.message || "Failed to send OTP.",
+      });
+      return
+    }
+  } catch (error) {
+    console.error("OTP sending error:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error." });
+    return;
+  }
+};
+
+// Verify OTP
+export const verifyAadhaarOTP = async (req: Request, res: Response, nex: NextFunction):Promise<void> => {
+  try {
+    // Validate Input
+    res
+      .status(200)
+      .json({ success: true, message: "OTP verified successfully!", data:{} });
+    return;
+    const parsedData = aadhaarOtpVerifySchema.safeParse(req.body);
+    if (!parsedData.success) {
+      res
+        .status(400)
+        .json({ success: false, message: parsedData.error.errors });
+      return;
+    }
+
+    const { aadhaarNumber, otp } = parsedData.data;
+
+    // Call Aadhaar Verifier API to verify OTP
+    const response = await fetch(
+      `${process.env.AADHAAR_VERIFIER_API_URL}/verify-otp`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.AADHAAR_API_KEY}`,
+        },
+        body: JSON.stringify({ aadhaarNumber, otp }),
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      res
+        .status(200)
+        .json({ success: true, message: "OTP verified successfully!", data });
+      return;
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: data.message || "Invalid OTP." });
+      return;
+    }
+  } catch (error) {
+    console.error("OTP verification error:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error." });
+    return;
   }
 };
