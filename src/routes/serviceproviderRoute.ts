6d39be7229169d59038bd8d248fa9b5d912b3d35
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2025-03-15 22:47:31 +0530

    Update serviceproviderRoute.ts

--- src/routes/serviceproviderRoute.ts
+++ src/routes/serviceproviderRoute.ts
@@ -15,10 +15,10 @@ import upload from "../middleware/upload";
 import { isAuthenticated } from "../middleware/authorised";
 const router = Router();
 router.post("/create", upload.single("providerpicture"), createServiceProvider);
-router.get("/all",isAuthenticated, getAllServiceProviders);
-router.get("/get/:id",isAuthenticated, getServiceProviderById);
+router.get("/all", getAllServiceProviders);
+router.get("/get",isAuthenticated, getServiceProviderById);
 router.put(
-  "/update/:id",
+  "/update",
   isAuthenticated,
   upload.single("serviceproviderpicture"),
   updateServiceProvider
@@ -26,8 +26,8 @@ router.put(
 router.delete("/delete/:id",isAuthenticated, deleteServiceProvider);
 router.get("/location/:id",isAuthenticated, getServiceProviderLocation);
 
-router.post("/create-availibility", createAvailability);
-router.put("/update-availibility", updateAvailability);
+router.post("/create-availibility",isAuthenticated, createAvailability);
+router.put("/update-availibility",isAuthenticated, updateAvailability);
 
 router.post("/otpverification", isAuthenticated, reachedAtUserLocation);
 
