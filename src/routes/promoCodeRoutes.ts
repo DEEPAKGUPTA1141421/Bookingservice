77379ba8ba4b862a9aca24a34691c09fd5b0d94d
DEEPAKGUPTA1141421 <103554042+DEEPAKGUPTA1141421@users.noreply.github.com>
2025-03-26 21:29:11 +0530

    Update promoCodeRoutes.ts

--- src/routes/promoCodeRoutes.ts
+++ src/routes/promoCodeRoutes.ts
@@ -13,7 +13,7 @@ import { authorizeRoles, isAuthenticated } from "../middleware/authorised";
 const router = Router();
 
 router.post("/create", createPromoCode);  // Create Promo Code
-router.get("/",isAuthenticated,authorizeRoles("user","admin") ,getPromoCodes);  // Get all Promo Codes
+router.get("/",getPromoCodes);  // Get all Promo Codes
 router.get("/:id",isAuthenticated,authorizeRoles("user","admin"), getPromoCodeById);  // Get Promo Code by ID
 router.put("/:id",isAuthenticated, authorizeRoles("admin"), updatePromoCode);  // Update Promo Code
 router.delete("/:id",isAuthenticated, authorizeRoles("admin"), deletePromoCode);  // Delete Promo Code
